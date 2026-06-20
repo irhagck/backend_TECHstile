5563ddec16c588fe39456a2cc3605a276dcff62f
aleesha079912 <alishabsit@gmail.com>
2026-06-20 12:08:49 +0500

    fix code

--- app/Http/Controllers/Api/FactoryController.php
+++ app/Http/Controllers/Api/FactoryController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Factory;
-
+use App\Models\Production;
+use Carbon\Carbon;
 class FactoryController extends Controller
 {
     // 1. Get all factories
