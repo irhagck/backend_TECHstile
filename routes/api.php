02ad949ba67ac7d56559997eb0eaaf8f87a42534
unknown <j8182067@gmail.com>
2026-09-07 05:28:56 -0700

    complete owner side

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ use App\Http\Controllers\Api\PaymentController;
 */
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/login-email', [AuthController::class, 'email']);
+// Route::post('/login-email', [AuthController::class, 'email']);
 
 Route::get('/employee/profile/{id}', [EmployeeDashController::class, 'profile']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
